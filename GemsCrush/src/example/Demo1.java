b402a2fcbd938efa9a78c4ec6d798edc86f335a1
haotianwu3 <496851081@qq.com>
2014-11-05 16:23:35 +0800

    default

---
+++ GemsCrush/src/example/Demo1.java
@@ -0,0 +1,91 @@
+/*
+ * Project Name: EE2311 Project - Gems Crush
+ * Student Name:
+ * Student ID:
+ * 
+ */
+
+package example;
+
+import game.GameConsole;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Point;
+
+/**
+ * Demo for the use of:
+ * 
+ * 1. create and display game console
+ * 2. start a game loop
+ * 3. create and display gems
+ * 4. detect mouse click and toggle gem's selection
+ * 5. update screen at predefined interval
+ * 6. draw text to show score/time information
+ *  
+ * @author Dr. Ray
+ */
+public class Demo1 {
+
+    // create the game console for drawing         
+    // singleton, always return the same instance
+    private GameConsole console = GameConsole.getInstance();
+
+    public Demo1() {
+        // make the console visible
+        console.show();
+    }
+
+    public static void main(String[] args) {
+        // a more OO approach to write the main method
+        Demo1 game = new Demo1();
+        game.startGame();
+    }
+
+    public void startGame() {
+
+        Gem gemBlue = new Gem("/assets/gemBlue.png", 2, 2);
+        Gem gemRed = new Gem("/assets/gemRed.png", 3, 3);
+
+        // board dimension can be obtained from console
+        int width = console.getBoardWidth();
+        int height = console.getBoardHeight();
+        
+        // set custom background image
+        console.setBackground("/assets/board.png");
+
+        // enter the main game loop
+        while (true) {
+
+            // get whatever inputs
+            Point point = console.getClickedPoint();
+            if (point != null) {
+                if (gemBlue.isAt(point)) {
+                    gemBlue.toggleFocus();
+                } else if (gemRed.isAt(point)) {
+                    gemRed.toggleFocus();
+                }
+            }
+
+            // refresh at the specific rate, default 25 fps
+            if (console.shouldUpdate()) {
+                console.clear();
+                
+                console.drawText(60, 150, "[TIME]", new Font("Helvetica", Font.BOLD, 20), Color.white);
+                console.drawText(60, 180, "00:05:32", new Font("Helvetica", Font.PLAIN, 20), Color.white);
+                
+                console.drawText(60, 250, "[SCORE]", new Font("Helvetica", Font.BOLD, 20), Color.white);
+                console.drawText(60, 280, "220", new Font("Helvetica", Font.PLAIN, 20), Color.white);
+                
+                gemBlue.display();
+                gemRed.display();
+
+                console.update();
+            }
+
+            // the idle time affects the no. of iterations per second which 
+            // should be larger than the frame rate
+            // for fps at 25, it should not exceed 40ms
+            console.idle(10);
+        }
+    }
+}
---
+++ GemsCrush/src/example/Gem.java
@@ -0,0 +1,93 @@
+/*
+ * Project Name: EE2311 Project - Gems Crush
+ * Student Name:
+ * Student ID:
+ * 
+ */
+
+package example;
+
+import game.GameConsole;
+import java.awt.Image;
+import java.awt.Point;
+import javax.swing.ImageIcon;
+
+/**
+ * Sample design of a toggle-enable gem
+ * 
+ * @author Dr. Ray
+ */
+public class Gem {
+
+    // the upper-left corner of the board, reference origin point
+    public static final int orgX = 240;
+    public static final int orgY = 40;
+    // the size of the gem
+    public static final int w = 65;
+    public static final int h = 65;
+    // default position in 8x8 grid    
+    private int posX = 0;
+    private int posY = 0;
+    private boolean selected = false;
+            
+    private Image pic;
+    private Image focus; 
+    
+    
+    Gem(String file, int x, int y) {
+        this.focus = new ImageIcon(this.getClass().getResource("/assets/focus.png")).getImage();
+        this.pic = new ImageIcon(this.getClass().getResource(file)).getImage();
+        this.posX = x;
+        this.posY = y;
+    }
+        
+    public void display() {
+        GameConsole.getInstance().drawImage((int)(posX * w + orgX), (int)(posY * h + orgY), pic);
+        if(selected)
+            GameConsole.getInstance().drawImage((int)(posX * w + orgX), (int)(posY * h + orgY), focus);
+    }
+    
+    public boolean isAt(Point point) {
+        if(point != null)
+            return (point.x > (posX * w + orgX) && point.x <= ((posX + 1) * w + orgX) && point.y > (posY * h + orgY) && point.y <= ((posY + 1) * h + orgY));   
+        else
+            return false;
+    }
+
+    public Image getPic() {
+        return pic;
+    }
+
+    public void setPic(String file) {
+        this.pic = new ImageIcon(this.getClass().getResource(file)).getImage();
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    
+    public void toggleFocus() {
+        selected = !selected;
+    }
+
+}
