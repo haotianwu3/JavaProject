01b1585e8d94dbf5b1348fb5fee441ab981c7ad0
juanayu <juanayuyue@gmail.com>
2014-11-14 14:13:32 +0800

    Elimination finished and regenerate finished

--- GemsCrush/src/example/Demo1.java
+++ GemsCrush/src/example/Demo1.java
@@ -13,6 +13,9 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.Point;
 import java.util.Random;
+import javax.swing.JButton;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  * Demo for the use of:
@@ -27,21 +30,22 @@ import java.util.Random;
  * @author Dr. Ray
  */
 public class Demo1 {
-
-    // create the game console for drawing         
-    // singleton, always return the same instance
     private GameConsole console = GameConsole.getInstance();
     private String score="0";
-
     public Demo1() {
         // make the console visible
         console.show();
     }
-
     public static void main(String[] args) {
         // a more OO approach to write the main method
-        Demo1 game = new Demo1();
-    
+      //adding button
+      Demo1 game = new Demo1();
+       /* ImageIcon start=new ImageIcon("iStock_000008997748XSmall.jpg");
+    JButton button1=new JButton(start);
+    JPanel panel=new JPanel();
+    panel.add(button1);
+*/
+
        game.startGame();
     }
 
@@ -49,15 +53,15 @@ public class Demo1 {
 //randomly generate 8*8 gems to fill up the whole board 
         //Blue = 1, Green = 2, Orange = 3, Purple = 4, Red = 5, White = 6, Yellow = 7
         Random rand = new Random();
-        int picNum = rand.nextInt(7)+1;
         boolean upSame = false;
         boolean leftSame = false;
+       
+       
         Gem gem[][]=new Gem[8][8];
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
                 gem[i][j]=new Gem("/assets/"+ (rand.nextInt(7)+1) + ".png", i, j);
-                //needed to be modified here
-                if(i == 0 && j>=1){
+                if(i == 0 && j>=1 ){
                     leftSame = checkLeft(gem[i][j],gem[i][j-1]);
                 }
                 if(i >= 1 && j == 0){
@@ -67,6 +71,7 @@ public class Demo1 {
                     leftSame = checkLeft(gem[i][j],gem[i][j-1]);
                     upSame = checkUp(gem[i][j], gem[i-1][j]);
                 }
+                
                 while(leftSame){
                     gem[i][j]=new Gem("/assets/"+ (rand.nextInt(7)+1) + ".png", i, j);
                     leftSame = checkLeft(gem[i][j],gem[i][j-1]);
@@ -98,7 +103,6 @@ public class Demo1 {
         int position_y2 = 0;
         // enter the main game loop
         while (true) {
-
             // get whatever inputs
             Point point = console.getClickedPoint();
             if (point != null && !firstClick) {
@@ -143,15 +147,11 @@ public class Demo1 {
                 console.drawText(60, 180, timeDisplay, new Font("Helvetica", Font.PLAIN, 20), Color.white);
                 // change the "220" score by a variable that accumulates from 0
                 console.drawText(60, 250, "[SCORE]", new Font("Helvetica", Font.BOLD, 20), Color.white);
-              
                 console.drawText(60, 280, score, new Font("Helvetica", Font.PLAIN, 20), Color.white);
-                
                 
                 for(int i=0;i<8;i++)
                     for(int j=0;j<8;j++)
                         gem[i][j].display();
-                   
-               
                 elimination(gem);
                 console.update();
             }
@@ -197,28 +197,34 @@ public class Demo1 {
     private String scoreAdd(int x){
         int score1=Integer.parseInt(score);
         score1=score1+10*x;
-       score = Integer.toString(score1);
-       return score;
+        score = Integer.toString(score1);
+        return score;
     }
 
     private void elimination(Gem[][] gem) {
         for(int i=0;i<8;i++){
             int endPlace = 0;
             int j = 0;
-            boolean finishRow = checkRow(gem, j, i, endPlace);
+           boolean finishRow = checkRow(gem, j, i, endPlace);
+        }
+        for(int i=0;i<8;i++){
+            int endPlace = 0;
+            int j = 0;
+         boolean finishCol = checkCol(gem, i,j, endPlace);
         }
     }
     private boolean checkRow(Gem [][] gem, int j, int i, int endPlace){
             endPlace = (checkRight(gem,i,j)+1);
-            // find that endPlace-j always samller than 2
-            if(endPlace-j>2){//here are some errors
-                //xiaochu j ~ endPlace
-                System.out.println(j);//used for testing
-                System.out.println(endPlace);//used for testing
+            if(endPlace-j>2){
                 scoreAdd(endPlace - j);
                 Random rand = new Random();
-                for(int start = j; start < endPlace; start++){
-                    gem[i][start] = new Gem("/assets/"+ (rand.nextInt(7)+1) + ".png", i, start);
+                //fall from top
+                for(int start = j; start < endPlace; start++){      
+                    for(int y=i;y>0;y--){
+                       gem[y-1][start].moveTo(y, start);
+                       gem[y][start]=new Gem(gem[y-1][start].getPic(),gem[y-1][start].getPosX(),gem[y-1][start].getPosY());
+                    }
+                       gem[0][start] = new Gem("/assets/"+ (rand.nextInt(7)+1) + ".png", 0,start);
                 }
             }
             else{
@@ -229,16 +235,52 @@ public class Demo1 {
                 checkRow(gem, j, i, endPlace);
             }
         return false;
-    
+    }
+    private boolean checkCol(Gem[][]gem,int j,int i,int endPlace){
+        endPlace=(checkDown(gem,i,j)+1);
+        int count=endPlace-i;
+        if(endPlace-i>2){
+            scoreAdd(endPlace-i);
+            Random rand = new Random();
+          //fall down
+            for(int start=endPlace-1;start>=count;start--){
+                       gem[start-count][j].moveTo(start,j);
+                       gem[start][j]=new Gem(gem[start-count][j].getPic(),gem[start-count][j].getPosX(),gem[start-count][j].getPosY());
+            }
+            for(int start=count-1;start>=0;start--)
+                gem[start][j] = new Gem("/assets/"+ (rand.nextInt(7)+1) + ".png", start,j);
+            }
+            
+        else{
+            if(endPlace>=7){
+                return true;
+            }
+            i=endPlace;
+            checkCol(gem,j,i,endPlace);
+        }
+        return false;
     }
     private int checkRight(Gem[][] gem, int i, int j) {
         int index = j;
         if(j == 7){
-            return 7;
+            index=7;
+           return 7;
         }
         if(checkLeft(gem[i][j],gem[i][j+1])){
             j += 1;
             index = checkRight(gem, i, j);
+        }
+        return index;
+    }
+    private int checkDown(Gem[][] gem,int i,int j){
+        int index=i;
+        if(i==7){
+            index=7;
+            return 7;
+        }
+        if(checkUp(gem[i+1][j],gem[i][j])){
+           i+=1;
+           index=checkDown(gem,i,j);
         }
         return index;
     }
